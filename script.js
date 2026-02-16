61027f15cd1c03843f3da3eb75cc542ccc756b34
Jonathan Tenório <joniws.tenorio@gmail.com>
2026-02-16 20:34:52 -0300

    Upgrade visual profissional

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function mostrarAba(id) {
-    const abas = document.querySelectorAll('.aba');
 
-    abas.forEach(function(aba) {
-        aba.classList.remove('ativa');
-    });
+    const abas = document.querySelectorAll('.aba');
+    const botoes = document.querySelectorAll('.menu button');
+
+    abas.forEach(aba => aba.classList.remove('ativa'));
+    botoes.forEach(botao => botao.style.background = "transparent");
 
     document.getElementById(id).classList.add('ativa');
+
+    event.target.style.background = "#38bdf8";
+    event.target.style.color = "black";
 }
